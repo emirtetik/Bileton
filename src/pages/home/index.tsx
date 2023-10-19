20c09f935b27f47d20ed0ea63df026847b895a78
Nedim <mr.ramicevic@gmail.com>
2023-10-19 15:54:33 +0300

    folder structure

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,13 @@
-import EventList from "../../components/eventList/eventList"
-
+import Carousel from "../../components/carousel";
+import EventsSection from "../../components/eventList/eventsSections";
 
 const Home = () => {
   return (
-    <div>
-    <EventList/>
+    <div className="flex flex-col px-8 gap-8">
+      <Carousel />
+      <EventsSection />
     </div>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
