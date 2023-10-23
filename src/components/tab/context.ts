7556fedd5eecc40d449dcb853cf46033e70cac98
Emir Tetik <info@frontmir.com>
2023-10-23 20:32:55 +0300

    context.ts dosyasında createContext'de beklenen argüman verildi.

--- src/components/tab/context.ts
+++ src/components/tab/context.ts
@@ -1,4 +1,4 @@
 import {createContext, useContext} from 'react'
 
-export const TabContext = createContext()
+export const TabContext = createContext("")
 export const useTab = () => {return useContext(TabContext)}
