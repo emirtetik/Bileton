30c81d2e3e16380749c0ed522c112fd62e5abea8
Nedim <mr.ramicevic@gmail.com>
2023-10-23 11:22:42 +0300

    dropdowns

---
+++ src/components/tab/tabHome/index.tsx
@@ -0,0 +1,34 @@
+import Sticky from "../../sticky";
+import Tab from "..";
+// import TodayProgram from "../../todayProgram"
+// import EventsSection from "../../eventList/eventsSections"
+import UpComing from "../../upcoming";
+import EventsSection from "../../eventList/eventsSections";
+
+const TabHome = () => {
+  return (
+    <div>
+      <Tab activeTab="home">
+        <Sticky>
+          <Tab.Items>
+            <Tab.Item id="home">Home</Tab.Item>
+            <Tab.Item id="past">Past</Tab.Item>
+          </Tab.Items>
+        </Sticky>
+        <Tab.Content id="home">
+          {/* Anasayfada bir component  gösterilicekse burda olmalı  */}
+          <UpComing />
+          <EventsSection />
+          {/* <TodayProgram/>
+           <EventsSection/> */}
+        </Tab.Content>
+        <Tab.Content id="past">
+          {/* Past sekmesinde bir component  gösterilicekse burda olmalı  */}
+          merhaba
+        </Tab.Content>
+      </Tab>
+    </div>
+  );
+};
+
+export default TabHome;
--- src/services/EventService.tsx
+++ src/services/EventService.tsx
@@ -1,6 +1,6 @@
 import { BaseService } from "./BaseService";
 
 export const EventService = {
-    getAll: async () => await BaseService.get("products"),
-    get: async (id: string) => await BaseService.get("events/" + id),
+  getAll: async () => await BaseService.get("products"),
+  get: async (id: string) => await BaseService.get("events/" + id),
 };
