418c5810f72318d7443d73ee39b0a77582b5bb1c
Nedim <mr.ramicevic@gmail.com>
2023-10-27 11:57:45 +0300

    eventDetailLink

--- src/components/eventDetail/index.tsx
+++ src/components/eventDetail/index.tsx
@@ -1,15 +1,37 @@
 import DetailSection from "./detailSection";
 import SummaryCard from "./summaryCard";
 import OtherEvents from "./otherEvents";
-
+import { useParams } from "react-router-dom";
+import { event } from "../../types";
+import useSWR from "swr";
+import { EventService } from "../../services/EventService";
+const fetcher = () => EventService.getAll();
+function slugify(str: string) {
+  return String(str)
+    .normalize("NFKD") // split accented characters into their base characters and diacritical marks
+    .replace(/[\u0300-\u036f]/g, "") // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+    .trim() // trim leading or trailing whitespace
+    .toLowerCase() // convert to lowercase
+    .replace(/[^a-z0-9 -]/g, "") // remove non-alphanumeric characters
+    .replace(/\s+/g, "-") // replace spaces with hyphens
+    .replace(/-+/g, "-"); // remove consecutive hyphens
+}
 const EventDetail = () => {
+  const { data, isLoading } = useSWR("eventByName", fetcher);
+  const { name } = useParams();
+
+  if (isLoading) return <div>Loading...</div>;
+  const cards = data.filter((event: event) =>
+    slugify(event.name).includes(slugify(name))
+  );
+
   return (
     <div className="h-screen ">
-      <SummaryCard />
-      <DetailSection />
+      <SummaryCard event={cards} />
+      <DetailSection event={cards} />
       <div className="bg-gray-100 ">
-      <OtherEvents />
-      </div> 
+        <OtherEvents />
+      </div>
     </div>
   );
 };
