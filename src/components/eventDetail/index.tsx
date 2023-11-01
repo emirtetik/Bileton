82f83de16e11af8c676327f5b807395c23bf8a1a
Nedim <mr.ramicevic@gmail.com>
2023-11-01 14:57:13 +0300

    data fetch diff

--- src/components/eventDetail/index.tsx
+++ src/components/eventDetail/index.tsx
@@ -5,25 +5,15 @@ import { useParams } from "react-router-dom";
 import { event } from "../../types";
 import useSWR from "swr";
 import { EventService } from "../../services/EventService";
-const fetcher = () => EventService.getAll();
-function slugify(str: string) {
-  return String(str)
-    .normalize("NFKD") // split accented characters into their base characters and diacritical marks
-    .replace(/[\u0300-\u036f]/g, "") // remove all the accents, which happen to be all in the \u03xx UNICODE block.
-    .trim() // trim leading or trailing whitespace
-    .toLowerCase() // convert to lowercase
-    .replace(/[^a-z0-9 -]/g, "") // remove non-alphanumeric characters
-    .replace(/\s+/g, "-") // replace spaces with hyphens
-    .replace(/-+/g, "-"); // remove consecutive hyphens
-}
+
 const EventDetail = () => {
+  const fetcher = () => EventService.getById(id);
+
   const { data, isLoading } = useSWR("eventByName", fetcher);
   const { name = "" } = useParams();
-
+  const id = name.split("-")[1];
   if (isLoading) return <div>Loading...</div>;
-  const cards = data.find((event: event) =>
-    slugify(event.name).includes(slugify(name.split("-")[0]))
-  );
+  const cards = data.find((event: event) => event._id === id);
   console.log(cards);
   cards.photos = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
   return (
