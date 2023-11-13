672cdd1f8e91b33f59b04d58aed23682e036353f
Nedim <mr.ramicevic@gmail.com>
2023-11-13 14:55:06 +0300

    flex items

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -5,68 +5,70 @@ import "react-datepicker/dist/react-datepicker.css";
 import { cardList } from "../../constant";
 import { NavLink, useMatch } from "react-router-dom";
 import { CiCircleList } from "react-icons/ci";
+import Card from "../../components/_coreComponent/card";
 
 const Calendar = () => {
-    const matchEvents = useMatch("/events");
-    const matchCalendar = useMatch("/calendar");
-const activeClassEvents = matchEvents  ? "text-yellow-500" : "";
-const activeClassCalendar = matchCalendar ? "text-yellow-500" : "";
+  const matchEvents = useMatch("/events");
+  const matchCalendar = useMatch("/calendar");
+  const activeClassEvents = matchEvents ? "text-yellow-500" : "";
+  const activeClassCalendar = matchCalendar ? "text-yellow-500" : "";
   const [selectedDate, setSelectedDate] = useState(new Date());
   const onChange = (date: Date) => {
     setSelectedDate(date);
   };
   return (
     <div className="bg-no-repeat bg-contain bg-background-image-4">
-        <div className="px-0 sm:px-20 pt-28 ">
-          <h1 className="mb-4 text-2xl font-extrabold text-white font-raleway">
-            Takvim
-          </h1>
-          <div className="flex items-center justify-end gap-4 px-10 mb-6 border-b border-white sm:py-2">
-          <NavLink to={"/events"} className={`text-white flex items-center text-lg ${activeClassEvents}`}>
+      <div className="px-0 sm:px-20 pt-28 ">
+        <h1 className="mb-4 text-2xl font-extrabold text-white font-raleway">
+          Takvim
+        </h1>
+        <div className="flex items-center justify-end gap-4 px-10 mb-6 border-b border-white sm:py-2">
+          <NavLink
+            to={"/events"}
+            className={`text-white flex items-center text-lg ${activeClassEvents}`}
+          >
             <CiCircleList className={`w-4 h-4 ${activeClassEvents}`} />
-              Liste
-            </NavLink>
-            <NavLink to={"/calendar"} className={`text-white flex items-center text-lg ${activeClassCalendar}`}>
+            Liste
+          </NavLink>
+          <NavLink
+            to={"/calendar"}
+            className={`text-white flex items-center text-lg ${activeClassCalendar}`}
+          >
             <BiCalendar className={`w-4 h-4 ${activeClassCalendar}`} />
-              Takvim
-            </NavLink>
+            Takvim
+          </NavLink>
+        </div>
+        {/* TAKVİM */}
+        <div className="flex flex-col-reverse md:flex-row">
+          <div className="w-4/5 p-4 ">
+            <div className=" flex flex-row flex-wrap justify-between">
+              {cardList.map((card, index: number) => (
+                <div className=" mb-20" key={index}>
+                  <Card
+                    title={card.title}
+                    image={card.img}
+                    date={card.date}
+                    time={card.time}
+                    venue={card.venue}
+                    size="big"
+                  />
+                </div>
+              ))}
+            </div>
           </div>
-          {/* TAKVİM */}
-          <div className="flex flex-col-reverse flex-wrap md:flex-row">
-            <div className="w-full p-4 md:w-2/3">
-              <div className="grid grid-cols-1 gap-4 md:grid-cols-3 gap-x-10">
-                {cardList.map((card, index: number) => (
-                  <div key={index}>
-                    <img
-                      src={card.img}
-                      alt={card.title}
-                      className="w-full h-[15rem] rounded-lg "
-                    />
-                    <div className="mt-1 text-left">
-                      <h2 className="font-medium truncate text-[18px]">
-                        {card.title}
-                      </h2>
-                      <p className="text-sm text-gray-500">{card.date}</p>
-                      <p className="text-sm text-gray-500">{card.time}</p>
-                      <p className="text-sm text-gray-500">{card.venue}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-            <div className="relative w-full p-4 text-right md:w-1/3">
-              <DatePicker
-                selected={selectedDate}
-                onChange={onChange}
-                minDate={new Date()}
-                inline
-                showDisabledMonthNavigation
-                className=""
-              />
-            </div>
+          <div className="relative w-1/5 p-4 text-right   ">
+            <DatePicker
+              selected={selectedDate}
+              onChange={onChange}
+              minDate={new Date()}
+              inline
+              showDisabledMonthNavigation
+              className=""
+            />
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -6,50 +6,54 @@ import { CiCircleList } from "react-icons/ci";
 import Card from "../../components/_coreComponent/card";
 import AosDiv from "../../components/_coreComponent/aosEffect";
 
-
-
 const Events = () => {
-    const matchEvents = useMatch("/events");
-    const matchCalendar = useMatch("/calendar");
-const activeClassEvents = matchEvents  ? "text-yellow-500" : "";
-const activeClassCalendar = matchCalendar ? "text-yellow-500" : "";
+  const matchEvents = useMatch("/events");
+  const matchCalendar = useMatch("/calendar");
+  const activeClassEvents = matchEvents ? "text-yellow-500" : "";
+  const activeClassCalendar = matchCalendar ? "text-yellow-500" : "";
   return (
     <div className="bg-no-repeat bg-contain bg-background-image-4">
-        <div className="px-0 sm:px-20 pt-28">
-          <h1 className="mb-4 text-2xl font-extrabold text-white font-raleway">
-            Etkinlikler
-          </h1>
-          <div className="flex items-center justify-end gap-4 px-10 mb-6 border-b border-white sm:py-2">
-            <NavLink to={"/events"} className={`text-white flex items-center text-lg ${activeClassEvents}`}>
+      <div className="px-0 sm:px-20 pt-28">
+        <h1 className="mb-4 text-2xl font-extrabold text-white font-raleway">
+          Etkinlikler
+        </h1>
+        <div className="flex items-center justify-end gap-4 px-10 mb-6 border-b border-white sm:py-2">
+          <NavLink
+            to={"/events"}
+            className={`text-white flex items-center text-lg ${activeClassEvents}`}
+          >
             <CiCircleList className={`w-4 h-4 ${activeClassEvents}`} />
-              Liste
-            </NavLink>
-            <NavLink to={"/calendar"} className={`text-white flex items-center text-lg ${activeClassCalendar}`}>
+            Liste
+          </NavLink>
+          <NavLink
+            to={"/calendar"}
+            className={`text-white flex items-center text-lg ${activeClassCalendar}`}
+          >
             <BiCalendar className={`w-4 h-4 ${activeClassCalendar}`} />
-              Takvim
-            </NavLink>
-          </div>
-          {/* LİSTE */}
+            Takvim
+          </NavLink>
+        </div>
+        {/* LİSTE */}
 
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-10">
-            {cardList.map((card, index: number) => (
-              <AosDiv aosType="zoom-in" aosDuration={500}>
-                <Card
-               key={index}
-               title={card.title}
-               image={card.img}
-               date={card.date}
-               time={card.time}
-               venue={card.venue}
-               size="medium"
-               className="text-left"
-              //  route={}
+        <div className=" flex flex-row flex-wrap justify-between">
+          {cardList.map((card, index: number) => (
+            <AosDiv aosType="zoom-in" aosDuration={500}>
+              <Card
+                key={index}
+                title={card.title}
+                image={card.img}
+                date={card.date}
+                time={card.time}
+                venue={card.venue}
+                size="big"
+                className="text-left"
+                //  route={}
               />
-              </AosDiv>
-            ))}
-          </div>
+            </AosDiv>
+          ))}
         </div>
       </div>
+    </div>
   );
 };
 
