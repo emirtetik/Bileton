0b3f0c83291d5f0c11bcdcc18a0b89ec73ff0acb
Nedim <mr.ramicevic@gmail.com>
2023-11-13 19:24:47 +0300

    css

--- src/pages/venue/index.tsx
+++ src/pages/venue/index.tsx
@@ -4,16 +4,16 @@ import { cardList } from "../../constant";
 import AosDiv from "../../components/_coreComponent/aosEffect";
 
 const Venue = () => {
-    const [sortType, setSortType] = useState("a-z");
+  const [sortType, setSortType] = useState("a-z");
 
-    const sortedCardList = [...cardList].sort((a, b) => {
-        switch (sortType) {
-          case "a-z":
-            return a.venue.localeCompare(b.venue);
-          default:
-            return 0;
-        }
-      });
+  const sortedCardList = [...cardList].sort((a, b) => {
+    switch (sortType) {
+      case "a-z":
+        return a.venue.localeCompare(b.venue);
+      default:
+        return 0;
+    }
+  });
   return (
     <div className="bg-no-repeat bg-contain bg-background-image-4">
       <div className="px-0 sm:px-20 pt-28">
@@ -21,7 +21,7 @@ const Venue = () => {
           Mekanlar ({cardList.length})
         </h1>
         <div className="flex items-center justify-end px-10 mb-6 border-b border-white sm:py-2">
-        <select
+          <select
             value={sortType}
             onChange={(e) => setSortType(e.target.value)}
             className="w-1/5 font-bold text-white bg-transparent border-b border-gray-300 outline-none text-text font-raleway"
@@ -29,21 +29,18 @@ const Venue = () => {
             <option value="a-z">A-Z</option>
           </select>
         </div>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-x-10">
+        <div className="flex flex-row flex-wrap justify-between">
           {sortedCardList.map((card, index: number) => (
-           <AosDiv 
-           aosType="zoom-in"
-           aosDuration={500}
-           >
-             <Card
-              key={index}
-              title={card.venue}
-              city={card.city}
-              image={card.img}
-              size="small"
-              className="text-left flex justify-center items-center flex-col"
-            />
-           </AosDiv>
+            <AosDiv aosType="zoom-in" aosDuration={500}>
+              <Card
+                key={index}
+                title={card.venue}
+                city={card.city}
+                image={card.img}
+                size="small"
+                className="text-left flex justify-center items-center flex-col"
+              />
+            </AosDiv>
           ))}
         </div>
       </div>
