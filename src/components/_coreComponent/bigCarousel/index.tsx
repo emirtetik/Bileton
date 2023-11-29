d2cb01964429e79add6009125d825bf557e0fe4b
Nedim <mr.ramicevic@gmail.com>
2023-11-29 12:43:15 +0300

    last changes

--- src/components/_coreComponent/bigCarousel/index.tsx
+++ src/components/_coreComponent/bigCarousel/index.tsx
@@ -1,6 +1,4 @@
 import { SwiperSlide } from "swiper/react";
-import { EventService } from "../../../services/EventService";
-import useSWR from "swr";
 import { event } from "../../../types";
 import { Navigation, Pagination, EffectCoverflow } from "swiper/modules";
 import CustomCarousel from "../../_coreComponent/customCarousel";
@@ -10,23 +8,8 @@ import "swiper/css/pagination";
 import "swiper/css/effect-coverflow";
 import Card from "../card";
 import AosDiv from "../aosEffect";
-const fetcher = () => EventService.getAll();
-const BigCarousel = (props: { events?: event[]; title?: string, filter?: (event: event) => boolean }) => {
-    const { data, error, isLoading } = useSWR("PopularEvent", fetcher);
-
-    if (isLoading) {
-      return <div>..Loading</div>;
-    }
-    if (error) {
-      return <div>...failed</div>;
-    }
-    let cards = data;
-    if (props.filter) {
-      cards = cards.filter(props.filter);
-    }
-    cards = cards.slice(-7);
-
-
+const BigCarousel = (props: { events: event[]; title: string }) => {
+  console.log("events", props);
   return (
     <div className="relative w-full px-4 my-6 text-gray-300 font-raleway">
       <div className="flex items-center justify-between py-4 ">
@@ -49,9 +32,9 @@ const BigCarousel = (props: { events?: event[]; title?: string, filter?: (event:
         className="populer-carousel"
       >
         <div className="w-full ">
-          {cards.map((card:event, index: number) => (
+          {props.events.map((card: event, index: number) => (
             <SwiperSlide key={index}>
-              <AosDiv aosType="fade-up" aosDuration={800} >
+              <AosDiv aosType="fade-up" aosDuration={800}>
                 <Card image={card.image} alt={card.name} size="large" />
               </AosDiv>
             </SwiperSlide>
--- src/components/artist/eventsByArtist.tsx
+++ src/components/artist/eventsByArtist.tsx
@@ -1,27 +1,27 @@
 import { useParams } from "react-router-dom";
 import useSWR from "swr";
 import { event } from "../../types";
-import { EventService } from "../../services/EventService";
 import Card from "../_coreComponent/card";
 import { slugify } from "../_coreComponent/slug";
+import { EventService } from "../../services/EventService";
 
 const fetcher = () => EventService.getAll();
 
-const EventsBycategory = () => {
-  const { data, isLoading, error } = useSWR("eventsbycategory", fetcher);
+const EventsByArtist = () => {
+  const { data, isLoading, error } = useSWR("artists1", fetcher);
   const { name = "" } = useParams();
   const lowerCaseName = name.toLowerCase();
 
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>Loading...</div>;
   const cards = data.filter(
-    (event: event) => event.category.toLowerCase() === lowerCaseName
+    (event: event) => event.artist.toLowerCase() === lowerCaseName
   );
 
   return (
     <div className="max-w-6xl px-0 mx-auto sm:px-20 pt-28">
       <h1 className="mb-4 text-2xl font-extrabold text-white font-raleway ">
-        {name[0].toUpperCase() + name.split("-")[0].slice(1)}
+        {name[0].toUpperCase() + name.slice(1)}
       </h1>
       <div className="flex items-center justify-end px-10 mb-6 border-b border-white sm:py-2"></div>
       <div className="flex flex-col justify-center w-full gap-4 md:gap-8 ">
@@ -41,4 +41,4 @@ const EventsBycategory = () => {
   );
 };
 
-export default EventsBycategory;
+export default EventsByArtist;
